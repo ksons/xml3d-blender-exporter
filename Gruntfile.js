fc1dfd159d5ec44662650a18979520d870d6bf55
Kristian Sons <kristian.sons@dfki.de>
2014-12-10 13:51:35 +0100

    Renamed TODO to TODO.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ exports = module.exports = function (grunt) {
     grunt.initConfig({
         todo: {
             options: {
-                file: "TODO", githubBoxes: true, colophon: false, usePackage: true
+                file: "TODO.md", githubBoxes: true, colophon: false, usePackage: true
             },
             src: ['addons/**/*.py']
         }
