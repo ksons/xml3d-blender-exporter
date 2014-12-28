70623160d50c65f691e6d11df1ef8fd6f467dd38
Kristian Sons <kristian.sons@dfki.de>
2014-12-28 13:29:11 +0100

    Initial version of d3 based visualization of resource sizes

--- addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js
+++ addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js
@@ -106,6 +106,7 @@ $(function () {
 
 
     $.get("./info/xml3d-info.json", function (data) {
+        $('#statisticsModal').foundation('reveal', 'open');
         getAnimationFrames(data.animations)
         var warnings = data.warnings;
         if (warnings.length) {
@@ -123,6 +124,14 @@ $(function () {
                 $("#bell").children(".message-position").remove();
             });
         }
+        createSunburst({
+            name: "scene",
+            children: [
+		{ name: "scene", size: data.scene.size },
+                { name: "assets", children: data.assets},
+                { name: "textures", children: data.textures}
+            ]
+        })
     });
 
 
@@ -172,4 +181,74 @@ $(function () {
     xml3d.appendChild(view.get(0));
     xml3d.setAttribute("activeView", "#v_pview");
 
+
+    function createSunburst(root) {
+
+        var width = 500, height = 500, radius = Math.min(width, height) / 2, color = d3.scale.category20c();
+
+        var svg = d3.select(".d3Target").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
+
+          var partition = d3.layout.partition()
+                            .sort(null)
+                            .size([2 * Math.PI, radius * radius])
+                            .value(function (d) {
+                                return d.size;
+                        });
+
+         var title = svg.append('text')
+            .text(root.name)
+            .attr('x', 0)
+            .attr('y', -5)
+            .style('font-size', '12px')
+            .style('fill', 'black')
+            .style('font-weight', 500)
+            .style('alignment-baseline', 'middle')
+            .style('text-anchor', 'middle')
+
+        var arc = d3.svg.arc().startAngle(function (d) {
+            return d.x;
+        }).endAngle(function (d) {
+            return d.x + d.dx;
+        }).innerRadius(function (d) {
+            return Math.sqrt(d.y);
+        }).outerRadius(function (d) {
+            return Math.sqrt(d.y + d.dy);
+        });
+
+            var path = svg.datum(root).selectAll("path").data(partition.nodes).enter().append("path").attr("display", function (d) {
+                return d.depth ? null : "none";
+            }) // hide inner ring
+                .attr("d", arc).style("stroke", "#000").style("fill", function (d) {
+                    return color((d.children ? d : d.parent).name);
+                }).style("fill-rule", "evenodd").each(stash);
+
+            d3.selectAll("input").on("change", function change() {
+                var value = this.value === "count" ? function () {
+                    return 1;
+                } : function (d) {
+                    return d.size;
+                };
+
+                path.data(partition.value(value).nodes).transition().duration(1500).attrTween("d", arcTween);
+            });
+
+        // Stash the old values for transition.
+        function stash(d) {
+            d.x0 = d.x;
+            d.dx0 = d.dx;
+        }
+
+        // Interpolate the arcs in data space.
+        function arcTween(a) {
+            var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+            return function (t) {
+                var b = i(t);
+                a.x0 = b.x;
+                a.dx0 = b.dx;
+                return arc(b);
+            };
+        }
+
+        d3.select(self.frameElement).style("height", height + "px");
+    };
 });
