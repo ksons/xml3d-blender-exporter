1426f398fb4d85fe9bfe542600eae37e400dd978
Kristian Sons <kristian.sons@dfki.de>
2015-01-31 16:12:06 +0100

    Put mesh computation into separate compilation unit

--- addons/io_scene_xml3d/export_asset.py
+++ addons/io_scene_xml3d/export_asset.py
@@ -1,19 +1,9 @@
 import os
-import operator
-import mathutils
 from xml.dom.minidom import Document
 from .export_material import Material, DefaultMaterial, export_image
 from bpy_extras.io_utils import create_derived_objects, free_derived_objects
 from . import tools
-
-
-def appendUnique(mlist, value):
-    if value in mlist:
-        return mlist[value], False
-    # Not in dict, thus add it
-    index = len(mlist)
-    mlist[value] = index
-    return index, True
+from . import meshtools
 
 
 class Asset:
@@ -147,107 +137,6 @@ class AssetExporter:
 
         return subasset_config
 
-    def get_bones_and_weights(self, groups, armature_info):
-        if not (len(groups) and armature_info):
-            return None, None
-
-        weights = []
-        group_index = mathutils.Vector.Fill(4, -1)
-        group_weights = mathutils.Vector.Fill(4, 0)
-
-        sum_weights = 0.0
-        for group in groups:
-            index = group.group
-            name = armature_info['vertex_groups'][index].name
-
-            if len(name) == 0:
-                print("No name")
-
-            if name not in armature_info['bone_map']:
-                # print("not in bone_map:", name)
-                continue
-
-            sum_weights += group.weight
-            weights.append((group.weight, armature_info['bone_map'][name]))
-
-        if sum_weights > 0:
-            # Now take the four with the highest influence
-            weights.sort(key=operator.itemgetter(0), reverse=True)
-            # Recalculate overall weight
-            sum_weights = 0.0
-            weight_count = len(weights)
-            for j in range(min(4, weight_count)):
-                weight = weights[j]
-                group_index[j] = weight[1]
-                group_weights[j] = weight[0]
-                sum_weights += weight[0]
-
-            group_weights *= 1.0 / sum_weights
-
-        else:
-            print("Found vertex without weights")
-
-        return group_index, group_weights
-
-    def export_tessfaces(self, mesh, armature_info):
-        if not len(mesh.tessfaces):
-            self.context.warning(u"Mesh '{0:s}' has no triangles. Pure line geometry not (yet) supported. Try extruding a little.".format(mesh.name), "geometry")
-            return None, None
-
-        materialCount = len(mesh.materials)
-
-        # Mesh indices:
-        # For each material allocate an array
-        # @UnusedVariable
-        indices = [[] for m in range(1 if materialCount == 0 else materialCount)]
-
-        # All vertices of the mesh, trying to keep the number of vertices small
-        vertices = []
-        # Vertex cache
-        vertex_dict = {}
-
-        uv_data = None
-        if len(mesh.tessface_uv_textures):
-            uv_data = [uv.data for uv in mesh.tessface_uv_textures]
-            uv_data = list(zip(*uv_data))
-
-        '''	@type bpytypes.MeshTessFace '''
-        faces = mesh.tessfaces
-        for faceIndex, face in enumerate(faces):
-
-            if uv_data:
-                ''' @type tuple(bpy.types.MeshTextureFace) '''
-                uv_faces = uv_data[faceIndex]
-                uv_vertices = [uv_face.uv for uv_face in uv_faces]
-                uv_vertices = list(zip(*uv_vertices))
-
-            faceIndices = []
-
-            for i, vertexIndex in enumerate(face.vertices):
-                normal = mesh.vertices[vertexIndex].normal if face.use_smooth else face.normal
-                uv_vertex = uv_vertices[i][0] if uv_data else None
-
-                group_index, group_weights = self.get_bones_and_weights(mesh.vertices[vertexIndex].groups, armature_info)
-
-                mv = tools.Vertex(vertexIndex, normal, uv_vertex, group_index, group_weights)
-
-                index, added = appendUnique(vertex_dict, mv)
-                faceIndices.append(index)
-                # print("enumerate: %d -> %d (%d)" % (i, vertexIndex, index))
-                if added:
-                    vertices.append(mv)
-
-            if len(faceIndices) == 3:
-                indices[face.material_index].extend(faceIndices)
-            elif len(faceIndices) == 4:
-                face2 = [faceIndices[2], faceIndices[3], faceIndices[0]]
-                faceIndices[3:] = face2
-                indices[face.material_index].extend(faceIndices)
-            else:
-                print("Found %s vertices" % len(faceIndices))
-
-        return vertices, indices
-
     def export_mesh_textures(self, mesh):
         textures = [None] * len(mesh.materials)
         for i, material in enumerate(mesh.materials):
@@ -264,7 +153,7 @@ class AssetExporter:
         materialCount = len(mesh.materials)
 
         # Export based on tess_faces:
-        vertices, indices = self.export_tessfaces(mesh, armature_info)
+        vertices, indices = meshtools.export_tessfaces(mesh, armature_info, self.context)
 
         if not (vertices and indices):
             return
