092bd7013f708cfe8f779a3f25107fa2a8a51575
Kristian Sons <kristian.sons@dfki.de>
2015-02-20 15:02:34 +0100

    Added client directory for template source files

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,25 @@ exports = module.exports = function (grunt) {
             options: {
                 file: "TODO.md", githubBoxes: true, colophon: false, usePackage: true
             }, src: ['addons/**/*.py']
+        },
+        browserify: {
+        preview: {
+            src: "client/templates/preview/index.js",
+            dest:  "addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js"
+        }
+        },
+        copy: {
+            preview: {
+                src:  "addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js",
+                dest: "output/public/scripts/xml3d-blender-preview.js"
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-todo');
+    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-flake8');
+      grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('templates', ['browserify', 'copy']);
 };
