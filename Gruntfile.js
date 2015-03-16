ce1117a7136c8081cd4353a54d213d2157dc468c
Kristian Sons <kristian.sons@dfki.de>
2015-03-16 09:51:30 +0100

    Added grunt compression target

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,24 +7,37 @@ exports = module.exports = function (grunt) {
             options: {
                 ignore: ['E501' // line too long
                 ]
-            }, src: ['addons/**/*.py']
+            },
+            src: ['addons/**/*.py']
         },
 
         todo: {
             options: {
                 file: "TODO.md", githubBoxes: true, colophon: false, usePackage: true
-            }, src: ['addons/**/*.py']
+            },
+            src: ['addons/**/*.py']
         },
+
         browserify: {
-        preview: {
-            src: "client/templates/preview/index.js",
-            dest:  "addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js"
-        }
+            preview: {
+                src: "client/templates/preview/index.js",
+                dest: "addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js"
+            }
         },
+
         copy: {
             preview: {
-                src:  "addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js",
+                src: "addons/io_scene_xml3d/templates/preview/public/scripts/xml3d-blender-preview.js",
                 dest: "output/public/scripts/xml3d-blender-preview.js"
+            }
+        },
+
+        compress: {
+            main: {
+                options: {
+                    mode: 'zip', archive: 'io_scene_xml3d-0.x.x.zip'
+                },
+                files: [{expand: true, cwd: 'addons/', src: ['**', '!**/__pycache__/**']}]
             }
         }
     });
@@ -32,7 +45,8 @@ exports = module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-todo');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-flake8');
-      grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
     grunt.registerTask('templates', ['browserify', 'copy']);
 };
